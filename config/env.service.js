bd2af4bc5f79602b02485d52290d78e66d33614a
YB122 <youssefbenyamine2eme@gmail.com>
2026-03-28 10:29:09 +0200

    edit in env

--- config/env.service.js
+++ config/env.service.js
@@ -7,10 +7,10 @@ let hash = process.env.HASH;
 let base_url = process.env.BASE_URL;
 let signatureAdmin = process.env.SIGNATURE_ADMIN;
 let signatureUser = process.env.SIGNATURE_USER;
-let accessToken = process.env.ACCESS_Token;
-let refreshToken = process.env.REFRESH_Token;
-let databaseUrl = process.env.DATA_BASE_URL_MY;
-let verifySignature = process.env.VERIFY_SIGNATURE_MY;
+let accessToken = process.env.ACCESS_TOKEN;
+let refreshToken = process.env.REFRESH_TOKEN;
+let databaseUrl = process.env.DATA_BASE_URL_Y;
+let verifySignature = process.env.VERIFY_SIGNATURE_Y;
 export const env = {
   port,
   email,
